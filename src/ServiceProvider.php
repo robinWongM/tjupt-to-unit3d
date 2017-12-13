f1e5728fdb5cc1437ea10839f60e4b80019e145e
pxgamer <owzie123@gmail.com>
2017-12-13 11:39:30 +0000

    Initialise ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace pxgamer\GazelleToUnit3d;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
