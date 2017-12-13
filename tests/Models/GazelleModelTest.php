eb973b1423fe85b1c2fc7157eacb928122a1bc90
pxgamer <owzie123@gmail.com>
2017-12-13 16:33:14 +0000

    Added tests

--- tests/Models/GazelleModelTest.php
+++ tests/Models/GazelleModelTest.php
@@ -13,13 +13,32 @@ class GazelleModelTest extends TestCase
     {
         $this->capsule = new Manager();
 
-        $this->capsule->addConnection([
-            'driver'    => 'sqlite',
-            'host'      => ':memory:',
-            'database'  => 'gazelle',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-        ]);
+        $this->capsule->addConnection(
+            [
+                'driver'    => env('DB_CONNECTION', 'mysql'),
+                'host'      => env('DB_HOST', '127.0.0.1'),
+                'port'      => env('DB_PORT', 3306),
+                'database'  => env('DB_DATABASE', 'unit3d'),
+                'username'  => env('DB_USERNAME', 'root'),
+                'password'  => env('DB_PASSWORD', 'root'),
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+            ]
+        );
+
+        $this->capsule->addConnection(
+            [
+                'driver'    => env('GAZELLE_DB_CONNECTION', 'mysql'),
+                'host'      => env('GAZELLE_DB_HOST', '127.0.0.1'),
+                'port'      => env('GAZELLE_DB_PORT', 3306),
+                'database'  => env('GAZELLE_DB_DATABASE', 'gazelle'),
+                'username'  => env('GAZELLE_DB_USERNAME', 'root'),
+                'password'  => env('GAZELLE_DB_PASSWORD', 'root'),
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+            ],
+            'gazelle'
+        );
     }
 
     public function testCanConstructGazelleModel()
@@ -27,5 +46,23 @@ class GazelleModelTest extends TestCase
         $gazelle = new GazelleModel($this->capsule);
 
         $this->assertInstanceOf(GazelleModel::class, $gazelle);
+    }
+
+    public function testCanImportAllUser()
+    {
+        $gazelle = new User($this->capsule);
+        $gazelle->importAll();
+    }
+
+    public function testCanImportAllUserInfo()
+    {
+        $gazelle = new UserInfo($this->capsule);
+        $gazelle->importAll();
+    }
+
+    public function testCanImportAllTorrent()
+    {
+        $gazelle = new Torrent($this->capsule);
+        $gazelle->importAll();
     }
 }
