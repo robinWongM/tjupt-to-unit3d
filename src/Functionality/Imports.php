57890e628afd06ba8ce17c9cc94f4e92beed2747
pxgamer <owzie123@gmail.com>
2017-12-15 20:35:48 +0000

    Split off Imports

---
+++ src/Functionality/Imports.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace pxgamer\GazelleToUnit3d\Functionality;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Imports
+ */
+class Imports
+{
+    /**
+     * @param Connection $database
+     * @param string $type
+     * @param string $oldTable
+     * @param string $modelName
+     *
+     * @return int
+     *
+     * @throws \ErrorException
+     */
+    public static function importTable(Connection $database, string $type, string $oldTable, string $modelName): int
+    {
+        $results = 0;
+
+        if (!$database->getSchemaBuilder()->hasTable($oldTable)) {
+            throw new \ErrorException('`'.$oldTable.'` table missing.');
+        }
+
+        $oldData = $database->query()->select()->from($oldTable)->get();
+
+        foreach ($oldData->all() as $oldDataItem) {
+            $data = Mapping::map($type, $oldDataItem);
+
+            if (self::import($modelName, $data)) {
+                $results++;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * @param string $model
+     * @param array $data
+     * @return bool
+     */
+    private static function import(string $model, array $data = []): bool
+    {
+        /** @var Model $new */
+        $new = new $model();
+
+        foreach ($data as $item => $value) {
+            $new->$item = $value;
+        }
+
+        return $new->save();
+    }
+}
